81c438763f4a83dee23061805c51768fac31f5cf
Adam Carter <adam@contra-design.com>
2019-09-22 16:34:59 +0100

    Created the title!

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w,"<h1>Base Hairdressing</h1>")
+}
 
 func main() {
-	fmt.Println("Hello!")
+	http.HandleFunc("/", home)
+
+	http.ListenAndServe(":8080", nil)
 }
