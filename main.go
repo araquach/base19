46998988fd35f073c9aa731bf591a3e367bbdea2
Adam Carter <adam@contra-design.com>
2019-09-25 10:15:23 +0100

    Added some views

--- main.go
+++ main.go
@@ -2,11 +2,31 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w,"<h1>Base Hairdressing</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	files := []string{
+		"./views/layouts/main.gohtml",
+		"./views/pages/home.gohtml",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err !=nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
@@ -19,5 +39,5 @@ func main() {
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 
-	http.ListenAndServe(":8080", r)
+	http.ListenAndServe(":8060", r)
 }
