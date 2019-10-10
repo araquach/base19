af806a2c675f3b898b85e783d3bca549dcf29f41
Adam Carter <adamcarter@jakatasalon.co.uk>
2019-10-10 17:51:02 +0100

    Switched the compnents over

---
+++ src/js/components/Main.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <home-component></home-component>
+
+        <about-component></about-component>
+
+        <!--<team-component></team-component>-->
+
+        <test-component></test-component>
+
+        <joinus-component></joinus-component>
+
+        <blog-component></blog-component>
+
+        <model-component></model-component>
+
+        <contact-component></contact-component>
+    </div>
+</template>
+
+<script>
+
+</script>
