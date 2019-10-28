7b2f94b378f26abad919dcc954c64b8e374030bc
Adam Carter <adam@contra-design.com>
2019-10-28 10:35:21 +0000

    Installed Vue scrollto and removed smooth scroll

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,4 @@
 import Vue from 'vue'
-import Buefy from 'buefy'
-import smoothscroll from 'smoothscroll-polyfill'
 import App from './App.vue'
 import Cookie from './components/Cookie'
 import Navbar from './components/layout/Navbar'
@@ -11,10 +9,12 @@ import Joinus from './components/joinus/Joinus'
 import Blog from './components/blog/Blog'
 import Model from './components/model/Model'
 import Contact from './components/contact/Contact'
-// test component
-import Test from './components/Test'
+
+import Buefy from 'buefy'
+import VueScrollTo from 'vue-scrollto'
 
 Vue.use(Buefy)
+Vue.use(VueScrollTo)
 
 Vue.component('navbar-component', Navbar)
 Vue.component('cookie-component', Cookie)
@@ -34,80 +34,4 @@ window.axios = require('axios');
 new Vue({
     el: '#app',
     render: h => h(App)
-})
-
-// kick off the polyfill!
-smoothscroll.polyfill();
-
-
-// add event listener on load
-window.addEventListener('load', function() {
-
-    // scroll to bottom
-    document.querySelector('.js-scroll-to-bottom').addEventListener('click', function(e) {
-        e.preventDefault();
-        window.scrollTo({ top: document.body.clientHeight - window.innerHeight, left: 0, behavior: 'smooth' });
-    });
-
-    // scroll to home
-    document.querySelector('.js-scroll-to-top').addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector('#home').scrollIntoView({ behavior: 'smooth' });
-    });
-
-    // scroll to about
-    document.querySelector('.js-scroll-to-about').addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector('#about').scrollIntoView({ behavior: 'smooth' });
-    });
-
-    // scroll to blog
-    document.querySelector('.js-scroll-to-blog').addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector('#blog').scrollIntoView({ behavior: 'smooth' });
-    });
-
-    // scroll to team
-    document.querySelector('.js-scroll-to-team').addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector('#team').scrollIntoView({ behavior: 'smooth' });
-    });
-
-    // scroll to joinus
-    document.querySelector('.js-scroll-to-joinus').addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector('#joinus').scrollIntoView({ behavior: 'smooth' });
-    });
-
-    // scroll to models
-    document.querySelector('.js-scroll-to-models').addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector('#models').scrollIntoView({ behavior: 'smooth' });
-    });
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-
-        // Add a click event on each of them
-        $navbarBurgers.forEach( el => {
-            el.addEventListener('click', () => {
-
-                // Get the target from the "data-target" attribute
-                const target = el.dataset.target;
-                const $target = document.getElementById(target);
-
-                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-                el.classList.toggle('is-active');
-                $target.classList.toggle('is-active');
-
-            });
-        });
-    }
-
 });
