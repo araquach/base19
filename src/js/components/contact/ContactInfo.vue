c2f11f41c3dbd96ff3b6761892095f233883e35c
Adam Carter <adamcarter@jakatasalon.co.uk>
2019-11-28 20:32:30 +0000

    implemented the validator

--- src/js/components/contact/ContactInfo.vue
+++ src/js/components/contact/ContactInfo.vue
@@ -1,58 +1,34 @@
 <template>
     <section id="contact" class="section contact-info hero is-fullheight is-dark">
-
-        <div v-if="formSubmitted">
-            <p class="is-size-4 has-text-primary">Thanks for messaging us! One of our team will get back to you soon.</p>
-        </div>
-        <form >
-            <div v-if="errors.length" class="box has-text-danger">
-                <p><strong>Please correct the following:</strong></p>
-                <ul>
-                    <li v-for="error in errors">{{ error }}</li>
-                </ul>
+        <form @submit.prevent="submit">
+            <div class="field" :class="{ 'has-text-danger': $v.name.$error }">
+                <label class="label has-text-white">Name</label>
+                <input class="input" v-model.trim="$v.name.$model"/>
             </div>
-
-            <div class="field">
-                <label class="label has-text-white">Full Name</label>
-                <div class="control">
-                    <input class="input" v-model="name" name="name" type="text" placeholder="Your Full Name">
-                </div>
-            </div>
-
-            <div class="field">
-                <label class="label has-text-white">Email Address</label>
-                <div class="control">
-                    <input class="input" v-model="email" name="email" type="text" placeholder="Your Email Address">
-                </div>
-            </div>
-
-            <div class="field">
-                <label class="label has-text-white">Message</label>
-                <div class="control">
-                    <input class="textarea" v-model="message" name="message" type="text" placeholder="Your Message">
-                </div>
-            </div>
-            <br>
-            <div class="field">
-                <div class="control">
-                    <button @click.prevent="sendMessage" class="button is-primary">Send message</button>
-                </div>
-            </div>
+            <div class="has-text-danger" v-if="!$v.name.required">Name is required</div>
+            <button class="button" type="submit" :disabled="submitStatus === 'PENDING'">Submit!</button>
+            <p v-if="submitStatus === 'OK'" cla>Thanks for your submission!</p>
+            <p v-if="submitStatus === 'ERROR'">Please fill the form correctly.</p>
+            <p v-if="submitStatus === 'PENDING'">Sending...</p>
         </form>
     </section>
 </template>
 
 <script>
+    import { required } from 'vuelidate/lib/validators'
+
     export default {
         data() {
             return {
-                showInfo: false,
-                errors: [],
-                name: null,
-                email: null,
-                message: null,
-                formSubmitted: false
+                name: '',
+                submitStatus: null
             }
+        },
+
+        validations: {
+            name: {
+                required,
+            },
         },
 
         methods:{
@@ -60,15 +36,18 @@
                 this.$emit('switchComponent')
             },
 
-            fullMessage() {
-                return `From: ${this.name}
-                Email Address: ${this.email}
-                Message: ${this.message}
-                `
-            },
-
-            sendMessage() {
-                this.formSubmitted = true
+            submit() {
+                console.log('submit!')
+                this.$v.$touch()
+                if (this.$v.$invalid) {
+                    this.submitStatus = 'ERROR'
+                } else {
+                    // do your submit logic here
+                    this.submitStatus = 'PENDING'
+                    setTimeout(() => {
+                        this.submitStatus = 'OK'
+                    }, 500)
+                }
             }
         }
     }
