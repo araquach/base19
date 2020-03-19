bf156f23233a4520505821a3aa436b1e5d4ee9f9
Adam Carter <adam@contra-design.com>
2020-03-19 11:20:21 +0000

    Changed the team individual

--- src/js/components/team/TeamModal.vue
+++ src/js/components/team/TeamModal.vue
@@ -1,26 +1,18 @@
 <template>
-    <div class="columns is-mobile is-centered">
-        <div class="column is-9">
-            <div class="card">
-                <div class="card-content columns">
-                    <div class="column">
-                        <figure class="image">
-                            <img :src="selectedTM.image" :alt="selectedTM.first_name">
-                        </figure>
-                    </div>
-                    <div class="column">
-                        <div class="media">
-                            <div class="media-content has-text-white">
-                                <h1 class="title has-text-white">{{selectedTM.first_name}} {{selectedTM.last_name}}</h1>
-                                <h2 class="subtitle has-text-white">{{selectedTM.level}}</h2>
-                                <p class="is-size-5 has-text-weight-bold">{{selectedTM.para_1}}</p>
-                                <p>{{selectedTM.para_2}}</p>
-                                <p>{{selectedTM.para_3}}</p>
-                                <p class="is-size-5">Average Price: &pound;{{selectedTM.price}}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    <div class="columns has-background-black">
+        <div class="section column">
+            <figure class="image">
+                <img :src="selectedTM.image" :alt="selectedTM.first_name">
+            </figure>
+        </div>
+        <div class="section column">
+            <div class="has-text-white">
+                <h1 class="title has-text-white">{{selectedTM.first_name}} {{selectedTM.last_name}}</h1>
+                <h2 class="subtitle has-text-white">{{selectedTM.level}}</h2>
+                <p class="is-size-5 has-text-weight-bold">{{selectedTM.para_1}}</p>
+                <p>{{selectedTM.para_2}}</p>
+                <p>{{selectedTM.para_3}}</p>
+                <p class="is-size-5">Average Price: &pound;{{selectedTM.price}}</p>
             </div>
         </div>
     </div>
