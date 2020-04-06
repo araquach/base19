4fd2fc17fc9d66b5367a7a66562b0058d16865fb
Adam Carter <adamcarter@jakatasalon.co.uk>
2020-04-06 21:44:25 +0100

    Added force SSL

--- main.go
+++ main.go
@@ -88,6 +88,19 @@ func init() {
 	}
 }
 
+func forceSsl(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if os.Getenv("GO_ENV") == "production" {
+			if r.Header.Get("x-forwarded-proto") != "https" {
+				sslUrl := "https://" + r.Host + r.RequestURI
+				http.Redirect(w, r, sslUrl, http.StatusTemporaryRedirect)
+				return
+			}
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	var err error
 	port := os.Getenv("PORT")
@@ -188,7 +201,7 @@ func main() {
 
 	log.Printf("Starting server on %s", port)
 
-	http.ListenAndServe(":" + port, r)
+	http.ListenAndServe(":" + port, forceSsl(r))
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
