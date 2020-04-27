c6b6a579047439821ad153a986f378a903858827
Adam Carter <adam@contra-design.com>
2020-04-27 20:07:50 +0100

    Got things working pretty good

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,7 +1,17 @@
 import Main from "./components/layout/Main"
 import AboutInfo from "./components/about/AboutInfo"
+import TeamInfo from "./components/team/TeamInfo"
+import OfferInfo from "./components/offer/OfferInfo"
+import JoinusInfo from "./components/joinus/JoinusInfo"
+import ModelInfo from "./components/model/ModelInfo"
+import ContactInfo from "./components/contact/ContactInfo"
 
 export const routes = [
     { path: '', component: Main },
-    { path: '/about', component: AboutInfo }
+    { path: '/about', component: AboutInfo },
+    { path: '/team', component: TeamInfo },
+    { path: '/offer', component: OfferInfo },
+    { path: '/joinus', component: JoinusInfo },
+    { path: '/model', component: ModelInfo },
+    { path: '/contact', component: ContactInfo }
 ]
