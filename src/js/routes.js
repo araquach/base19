1a9ca73cb92f428c4e7147c108fd16e5e6c91fab
Adam Carter <adam@contra-design.com>
2020-04-28 11:57:18 +0100

    Fixed the problem

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,4 +1,5 @@
 import Main from "./components/layout/Main"
+import HomeInfo from "./components/home/HomeInfo"
 import AboutInfo from "./components/about/AboutInfo"
 import TeamInfo from "./components/team/TeamInfo"
 import OfferInfo from "./components/offer/OfferInfo"
