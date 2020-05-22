911070f147d041046e23e88e77b901c2d88f2357
Adam Carter <adam@contra-design.com>
2020-05-22 16:38:32 +0100

    Added social Links

--- src/js/components/home/HomeInfo.vue
+++ src/js/components/home/HomeInfo.vue
@@ -13,7 +13,29 @@
                 <h2 class="subtitle is-size-4 has-text-white has-text-centered">
                     A hair academy for the next generation of super skilled stylists
                 </h2>
-
+                <div class="columns social">
+                    <div class="column is-2 is-offset-3">
+                        <figure class="image is-square">
+                            <a href="https://www.instagram.com/basehairdressing/">
+                                <img src="/dist/img/home/social/insta.svg" alt="Instagram">
+                            </a>
+                        </figure>
+                    </div>
+                    <div class="column is-2">
+                        <figure class="image is-square">
+                            <a href="https://www.facebook.com/basehairdressing/">
+                                <img src="/dist/img/home/social/fb.svg" alt="Facebook">
+                            </a>
+                        </figure>
+                    </div>
+                    <div class="column is-2">
+                        <figure class="image is-square">
+                            <a href="https://www.pinterest.co.uk/basehairdressing/">
+                                <img src="/dist/img/home/social/pinterest.svg" alt="Pinterest">
+                            </a>
+                        </figure>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="has-text-right">
