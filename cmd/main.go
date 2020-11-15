07fb356779c956ff9e357b7994840a6afecc3968
araquach <adam@contra-design.com>
2020-11-15 20:15:37 +0000

    Got the new setup almost working

--- cmd/main.go
+++ cmd/main.go
@@ -1,75 +1,7 @@
 package main
 
-import (
-	"flag"
-	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/joho/godotenv"
-	"html/template"
-	"log"
-	"net/http"
-	"os"
-)
-
-var (
-	tpl *template.Template
-)
-
-func dbConn() (db *gorm.DB) {
-	db, err := gorm.Open("postgres", os.Getenv("DATABASE_URL"))
-	if err != nil {
-		panic(err)
-	}
-	return db
-}
-
-func init() {
-	// loads values from .env into the system
-	if err := godotenv.Load(); err != nil {
-		log.Print("No .env file found")
-	}
-}
+import "github.com/araquach/salonserver"
 
 func main() {
-	var err error
-	var dir string
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
-
-	db := dbConn()
-	db.LogMode(true)
-	db.AutoMigrate(&TeamMember{}, &JoinusApplicant{}, &ModelApplicant{}, &Review{}, &MetaInfo{}, &BookingRequest{})
-	db.Close()
-
-	tpl = template.Must(template.ParseFiles(
-		"views/index.gohtml"))
-	if err != nil {
-		panic(err)
-	}
-
-	flag.StringVar(&dir, "dir", "dist", "the directory to serve files from")
-	flag.Parse()
-	r := mux.NewRouter()
-
-	r.PathPrefix("/dist/").Handler(http.StripPrefix("/dist/", http.FileServer(http.Dir(dir))))
-	r.HandleFunc("/api/team", apiTeam)
-	r.HandleFunc("/api/team/{slug}", apiTeamMember)
-	r.HandleFunc("/api/sendMessage", apiSendMessage)
-	r.HandleFunc("/api/joinus", apiJoinus)
-	r.HandleFunc("/api/models", apiModel)
-	r.HandleFunc("/api/reviews/{tm}", apiReviews)
-	r.HandleFunc("/api/booking-request", apiBookingRequest)
-	r.HandleFunc("/api/blogpost/{slug}", apiBlogPost).Methods("GET")
-	r.HandleFunc("/api/blogposts", apiBlogPosts).Methods("GET")
-	r.HandleFunc("/{category}/{name}", home)
-	r.HandleFunc("/{name}", home)
-	r.HandleFunc("/", home)
-
-	log.Printf("Starting server on %s", port)
-
-	http.ListenAndServe(":" + port, forceSsl(r))
+	salonserver.Serve(3)
 }
--- go.mod
+++ go.mod
@@ -2,10 +2,4 @@ module contra-design.com/base19
 
 go 1.12
 
-require (
-	github.com/gorilla/mux v1.7.3
-	github.com/jinzhu/gorm v1.9.11
-	github.com/joho/godotenv v1.3.0
-	github.com/mailgun/mailgun-go/v3 v3.6.0
-	github.com/russross/blackfriday v1.5.2
-)
+require github.com/araquach/salonserver v0.0.4
