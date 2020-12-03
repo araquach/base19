2f556272a65704d90aba0d8b7e39589830e83c23
Adam Carter <adam@contra-design.com>
2020-12-03 20:01:09 +0000

    Turned online booking back on

--- src/js/components/layout/Navbar.vue
+++ src/js/components/layout/Navbar.vue
@@ -33,12 +33,9 @@
         <template slot="end">
             <b-navbar-item tag="div">
                 <div class="buttons">
-<!--                    <a href="https://phorest.com/book/salons/basehairacademy" class="button is-primary">-->
-<!--                        <strong>Book Now</strong>-->
-<!--                    </a>-->
-                  <b-navbar-item tag="router-link" class="button is-primary" :to="{ name: 'bookings' }" v-scroll-to="'#contact'">
-                    Bookings
-                  </b-navbar-item>
+                  <a href="https://phorest.com/book/salons/basehairacademy" class="button is-primary">
+                      <strong>Book Now</strong>
+                  </a>
                   <b-navbar-item tag="router-link" class="button is-light" to="/#contact" v-scroll-to="'#contact'">
                       Contact Us
                   </b-navbar-item>
