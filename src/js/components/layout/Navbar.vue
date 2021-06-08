feedae4a23a9489e5108841e26ef8eb7b814bb0f
araquach <adam@contra-design.com>
2021-06-08 10:35:43 +0100

    Added offers link

--- src/js/components/layout/Navbar.vue
+++ src/js/components/layout/Navbar.vue
@@ -1,55 +1,47 @@
 <template>
-    <b-navbar class="is-fixed-top is-spaced">
-        <template slot="start">
-            <b-navbar-item tag="router-link" :to="{ name: 'main', hash: '#home' }" v-scroll-to="'#home'">
-                Home
-            </b-navbar-item>
-            <a href="" ></a>
-            <b-navbar-item tag="router-link" :to="{ name: 'main', hash: '#about' }" v-scroll-to="'#about'">
-                About
-            </b-navbar-item>
-            <b-navbar-item tag="router-link" :to="{ name: 'main', hash: '#team' }" v-scroll-to="'#team'">
-                Team
-            </b-navbar-item>
-            <b-navbar-item tag="router-link" :to="{ name: 'main', hash: '#blog' }" v-scroll-to="'#blog'">
-                Blog
-            </b-navbar-item>
-            <b-navbar-item tag="router-link" :to="{ name: 'main', hash: '#price' }" v-scroll-to="'#price'">
-              Prices
-            </b-navbar-item>
-<!--            <b-navbar-item tag="router-link" to="/#corona" v-scroll-to="'#corona'">-->
-<!--                Corona-->
-<!--            </b-navbar-item>-->
-<!--            <b-navbar-item tag="router-link" to="/#offers" v-scroll-to="'#offers'">-->
-<!--              Offers-->
-<!--            </b-navbar-item>-->
-            <b-navbar-dropdown label="More">
-<!--                <b-navbar-item tag="router-link" :to="{ name: 'prices' }">-->
-<!--                  Prices-->
-<!--                </b-navbar-item>-->
-                <b-navbar-item tag="router-link" :to="{ name: 'main', hash: '#joinus' }" v-scroll-to="'#joinus'">
-                    Join Us
-                </b-navbar-item>
-                <hr class="navbar-divider">
-                <b-navbar-item tag="router-link" :to="{ name: 'main', hash: '#models' }" v-scroll-to="'#models'">
-                    Models
-                </b-navbar-item>
-            </b-navbar-dropdown>
-        </template>
-        <template slot="end">
-            <b-navbar-item tag="div">
-                <div class="buttons">
-                  <a href="https://phorest.com/book/salons/basehairacademy" class="button is-primary">
-                      <strong>Book Now</strong>
-                  </a>
-<!--                  <b-navbar-item tag="router-link" class="button is-primary" :to="{ name: 'bookings' }">-->
-<!--                    Book Now-->
-<!--                  </b-navbar-item>-->
-                  <b-navbar-item tag="router-link" class="button is-light" :to="{ name: 'contact', hash: '#contact' }" v-scroll-to="'#contact'">
-                      Contact Us
-                  </b-navbar-item>
-                </div>
-            </b-navbar-item>
-        </template>
-    </b-navbar>
+  <b-navbar class="is-fixed-top is-spaced">
+    <template slot="start">
+      <b-navbar-item v-scroll-to="'#home'" :to="{ name: 'main', hash: '#home' }" tag="router-link">
+        Home
+      </b-navbar-item>
+      <a href=""></a>
+      <b-navbar-item v-scroll-to="'#about'" :to="{ name: 'main', hash: '#about' }" tag="router-link">
+        About
+      </b-navbar-item>
+      <b-navbar-item v-scroll-to="'#team'" :to="{ name: 'main', hash: '#team' }" tag="router-link">
+        Team
+      </b-navbar-item>
+      <b-navbar-item v-scroll-to="'#blog'" :to="{ name: 'main', hash: '#blog' }" tag="router-link">
+        Blog
+      </b-navbar-item>
+      <b-navbar-item v-scroll-to="'#offers'" tag="router-link" to="/#offers">
+        Offers
+      </b-navbar-item>
+      <b-navbar-dropdown label="More">
+        <b-navbar-item v-scroll-to="'#price'" :to="{ name: 'main', hash: '#price' }" tag="router-link">
+          Prices
+        </b-navbar-item>
+        <b-navbar-item v-scroll-to="'#joinus'" :to="{ name: 'main', hash: '#joinus' }" tag="router-link">
+          Join Us
+        </b-navbar-item>
+        <hr class="navbar-divider">
+        <b-navbar-item v-scroll-to="'#models'" :to="{ name: 'main', hash: '#models' }" tag="router-link">
+          Models
+        </b-navbar-item>
+      </b-navbar-dropdown>
+    </template>
+    <template slot="end">
+      <b-navbar-item tag="div">
+        <div class="buttons">
+          <a class="button is-primary" href="https://phorest.com/book/salons/basehairacademy">
+            <strong>Book Now</strong>
+          </a>
+          <b-navbar-item v-scroll-to="'#contact'" :to="{ name: 'contact', hash: '#contact' }" class="button is-light"
+                         tag="router-link">
+            Contact Us
+          </b-navbar-item>
+        </div>
+      </b-navbar-item>
+    </template>
+  </b-navbar>
 </template>
