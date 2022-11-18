048964fbac43bec7963a84ace9957274afff4e54
araquach <adam@contra-design.com>
2022-11-18 12:20:04 +0000

    Added Base

--- src/js/views/GhdPromo.vue
+++ src/js/views/GhdPromo.vue
@@ -2,6 +2,7 @@
   <section class="section ghdPromo hero is-fullheight is-dark">
     <div class="columns">
       <div class="column is-8">
+        <p class="is-size-6"><em>You're invited to our</em></p>
         <img src="/dist/img/ghdPromo/ghd_logo.png" alt="GHD" width="250">
         <h1 class="title is-2">Black Wednesday Event</h1>
         <p class="is-size-5">Do you struggle to know how to use your hot tools to get the best results? Or are you unsure about which tool is the right choice for you?</p>
@@ -13,7 +14,11 @@
         </figure>
         <br>
         <p class="is-size-4">We're also offering <strong>£20 off</strong> selected GHD lines to either treat yourself or someone else for Christmas this year!</p>
-        <p class="is-size-4 has-text-warning"><em>We look forward to seeing you on Wednesday 23rd! Feel free to call in anytime between 4pm and 8pm!</em></p>
+        <p class="is-size-4 has-text-warning"><em>We look forward to seeing you on Wednesday 23rd at</em></p>
+        <img src="dist/img/base_logo.svg" alt="Base Hairdressing" width="200">
+        <br><br>
+        <p>90/02 Bridge Street, WA1 2RF</p>
+        <p class="is-size-4 has-text-warning"><em>Feel free to call in anytime between 4pm and 8pm! Bring a friend or two if you like!</em></p>
       </div>
       <div class="column">
 
