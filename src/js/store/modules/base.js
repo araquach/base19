40c77adb6e8c81da3df16b5f21d2b59f71d8b93e
araquach <adam@contra-design.com>
2022-12-30 09:44:56 +0000

    Removed Offers

--- src/js/store/modules/base.js
+++ src/js/store/modules/base.js
@@ -5,7 +5,7 @@ import {addWeeks} from "date-fns";
 const today = new Date()
 
 export const state = {
-    hideOffers: false,
+    hideOffers: true,
     newsItems: [],
     endDate: format(addWeeks(today, 4), "dd/MM/yy")
 }
