3cb08c0cb58fcb1eb7f085219ab5e73c8e7a5ba6
araquach <adam@contra-design.com>
2024-04-03 06:10:09 +0100

    Updated

--- src/js/views/KatieMove.vue
+++ src/js/views/KatieMove.vue
@@ -2,13 +2,26 @@
   <section class="section hour-changes hero is-fullheight is-dark">
     <div class="columns">
       <div class="column is-8">
-        <h1 class="title">Katie is moving to Jakata!</h1>
-
-        <p class="is-size-5"><em>If you have any questions don't hesitate to get in touch.</em></p>
-        <p>Call the salon on 01925 444449</p>
+        <h1 class="title">Exciting News: Katie's Moving to Jakata Salon!</h1>
+        <p class="is-size-5">We're thrilled to announce that <strong>Katie</strong>, who joined us as a Graduate Stylist in March 2023, is taking a significant step in her career by joining our sister salon,
+          <strong>Jakata</strong>.</p>
+        <p>Katie joined Base to polish up her skills before making a move to one of our other sites. She's now ready to continue her career progression at
+          <strong>Jakata</strong>, where she will bring her dedication and exceptional styling services.</p>
+        <h2 class="title is-4">Book Your Next Appointment with Katie at Jakata!</h2>
+        <p>Katie is eager to welcome you at <strong>Jakata</strong> as she continues her journey. We're excited for you to experience her great styling in this new chapter.</p>
+        <p><strong>The great news is, Jakata is just around the corner from Base, making it easy for you to follow Katie in her next step. Here’s where you can find her:</strong></p>
+        <p class="is-size-5">Jakata Salon <br>27 Rylands Street <br>Warrington <br> WA1 1SL</p>
+        <p class="is-size-5">Telephone: 01925 242960</p>
+        <p class="is-size-5"><em>If you already have an appointment booked with Katie, it will be automatically transferred to Jakata.</em></p>
+        <p class="is-size-3">Thank You for Your Support!</p>
+        <p>We're proud of Katie's development and grateful for your continued support. <strong>See you soon at Jakata!</strong></p>
+        <p class="is-size-5"><em>If you have any questions, don't hesitate to get in touch.</em></p>
+        <p class="is-size-4">Call Base on 01925 444449</p>
+        <a class="has-text-primary is-size-4" href="https://www.jakatasalon.co.uk">Find out more about Jakata Here...</a>
       </div>
       <div class="column is-4">
-        <img alt="Katy" src="/dist/img/team/profiles/katie_1.jpg">
+        <!-- Replace the 'src' attribute value with Katie's profile image path -->
+        <img alt="Katie" src="/dist/img/team/profiles/katie_1.jpg">
       </div>
     </div>
   </section>
--- src/js/views/SarahMove.vue
+++ src/js/views/SarahMove.vue
@@ -3,20 +3,23 @@
     <div class="columns">
       <div class="column is-8">
         <h1 class="title">Exciting News: Sarah's Moving to Jakata Salon!</h1>
-        <p class="is-size-5">We're delighted to share that Sarah is taking a significant step in her career by joining our sister salon, Jakata, from the 9th of April.</p>
-        <p>Sarah has excelled in her apprenticeship, earning a distinction and proving her skills as a junior stylist. Her dedication to her craft has prepared her for this exciting new role at Jakata, where she will continue to offer her exceptional styling services.</p>
+        <p class="is-size-5">We're delighted to share that <strong>Sarah</strong> is taking a significant step in her career by joining our sister salon,
+          <strong>Jakata</strong>, from the 9th of April.</p>
+        <p>Sarah has excelled in her apprenticeship, earning a <strong>distinction</strong> and proving her skills as a junior stylist. Her dedication to her craft has prepared her for this exciting new role at
+          <strong>Jakata</strong>, where she will continue to offer her exceptional styling services.</p>
         <h2 class="title is-4">Book Your Next Appointment with Sarah at Jakata!</h2>
-        <p>Starting April 9th, all of her appointments will be at Jakata. We're eager for you to join us in this new chapter and experience the same great styling by Sarah in a new setting.</p>
+        <p>Starting <strong>April 9th</strong>, all of her appointments will be at <strong>Jakata</strong>. We're eager for you to join us in this new chapter and experience the same great styling by Sarah in a new setting.</p>
         <p><strong>The great news is you don’t have to travel far to find Sarah - Jakata is just around the corner from
           Base. You can find the salon here:</strong></p>
-        <p class="is-size-5">27 Rylands Street <br>Warrington <br> WA1 1SL</p>
+        <p class="is-size-5">Jakata Salon <br>27 Rylands Street <br>Warrington <br> WA1 1SL</p>
+        <p class="is-size-5">Telephone: 01925 242960</p>
         <p class="is-size-5"><em>If you already have an appointment with Sarah it will be automatically transferred over to Jakata.</em></p>
         <p class="is-size-3">Thank You for Your Support!</p>
         <p>We're proud of Sarah's growth and grateful for your continued support, which makes these steps forward
-          possible. See you soon at Jakata!</p>
+          possible. <strong>See you soon at Jakata!</strong></p>
         <p class="is-size-5"><em>If you have any questions don't hesitate to get in touch.</em></p>
-        <p class="is-size-4">Call us on 01925 444449</p>
-
+        <p class="is-size-4">Call Base on 01925 444449</p>
+        <a class="has-text-primary is-size-4" href="https://www.jakatasalon.co.uk">Find out more about Jakata Here...</a>
       </div>
       <div class="column is-4">
         <img alt="Sarah" src="/dist/img/team/profiles/sarah_1.jpg">
