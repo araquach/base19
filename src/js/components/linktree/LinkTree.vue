e856b012fbeab6a612cf94c2f7a2828a1393091f
araquach <adam@contra-design.com>
2024-06-04 14:36:00 +0100

    Added Skin Test link to links

--- src/js/components/linktree/LinkTree.vue
+++ src/js/components/linktree/LinkTree.vue
@@ -8,6 +8,9 @@
     </div>
     <div class="section columns is-centered is-mobile">
       <div class="column is-9 has-text-centered is-size-5">
+        <a v-if="$route.path === '/links'" href="https://www.salonhub.uk/skintest-renew/3" class="box lt-blog">
+          <span class="title-new-mid has-text-white">SKIN TEST UPDATE</span>
+        </a>
         <router-link :to="{ name: 'referrals' }" class="box lt-refer">
           <span class="title-new-mid has-text-white">REFER A FRIEND</span>
         </router-link>
@@ -64,5 +67,3 @@
     </div>
   </div>
 </template>
-<script setup>
-</script>
