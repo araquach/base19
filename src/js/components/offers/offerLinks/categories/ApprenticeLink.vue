11b805ec6a84103ef1ff1a77b66fd9de3d0cc0bb
araquach <adam@contra-design.com>
2026-01-30 16:45:29 +0000

    Fixed Price

--- src/js/components/offers/offerLinks/categories/ApprenticeLink.vue
+++ src/js/components/offers/offerLinks/categories/ApprenticeLink.vue
@@ -8,7 +8,7 @@
         <section class="column is-8">
           <h2 class="title is-3 has-text-white">Ready for a fresh new look?</h2>
           <p class="is-size-4">Special offer with Brooke, Molly and Maisie</p>
-          <p>A Colour & Cut package for just £50 for new clients</p>
+          <p>A Colour & Cut package for just £55 for new clients</p>
           <p class="is-size-4">Find out more here ></p>
         </section>
       </div>
