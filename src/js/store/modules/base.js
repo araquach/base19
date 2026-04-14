fa4da2d4a1591ea17d90d77a455923cafed2bb58
araquach <adam@contra-design.com>
2026-04-14 12:24:48 +0100

    Updated date

--- src/js/store/modules/base.js
+++ src/js/store/modules/base.js
@@ -7,7 +7,7 @@ export const state = {
     userPin: window.localStorage.getItem('userPin') || "",
     hideOffers: false,
     newsItems: [],
-    endDate: "31/03/26",
+    endDate: "30/04/26",
     applicants: [],
     applicant: {},
     showAllApplicants: false,
